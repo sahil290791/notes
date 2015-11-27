6456462aa507573dc3825b99a3265ee8ad306017
Sahil <sahil.prajapati@tcs.com>
2015-11-27 12:19:47 +0530

    app now running

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127064218) do
+ActiveRecord::Schema.define(version: 20151127064832) do
+
+  create_table "notes", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
